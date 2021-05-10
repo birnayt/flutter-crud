818348153849fe03f0c97cd92542c3d0780e7ee5
LucasFr <lucasfariasrodrigues25@gmail.com>
2021-05-09 21:08:28 -0300

    my first project flutter, a simple crud of users list

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'Fernanda@zymail.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/26/07/57/woman-1353825_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guigui@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png'),
+};
